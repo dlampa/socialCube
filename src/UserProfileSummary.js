a67de3b48b4e35f62eb5a1e4b84c009ce1082c9b
unknown <sbarre33@gmail.com>
2020-08-03 22:32:08 -0600

    added pseudo-code to structure html elements. Sent for review. Did not add to master

---
+++ src/UserProfileSummary.js
@@ -0,0 +1,25 @@
+/*import React from 'react';
+import UserProfilePage from './UserProfilePage';
+
+class UserProfileSummary extends React.Component 
+{
+    //in <h1> populate userName props passed in from UserProfilePage file. Use proper syntax.
+    //in <p>  populate userDescription, userEmail and userBirthday props passed in from UserProfilePage file. 
+    // and use <span> inside each paragraph to seperate content for styling purposes. Use proper syntax.
+    //added <a> href element to create a link for the userEmail so that it's clickable and can go straight to email. 
+
+    render()
+    {
+        return( 
+            <section>
+            <h1><span class="userName">userName userSurname</span></h1>
+            <p><span class="userDescription">"Climate and Environmental Activist"</span></p>
+            <p><span class=userEmail><a><href="mailto:{name.surname@gmail.com}"/></span></p>
+            <p><span class= "userBirthday">"Bday: April 1, 1985" </span></p>
+            </section>  
+        );
+    }
+}
+export default UserProfileSummary 
+*/
+    
